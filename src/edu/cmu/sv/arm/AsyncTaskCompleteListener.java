b33309907cb267207609bcf887a0345a4e2a2a24
Maria Emilia Torino <emilia.torino@west.cmu.edu>
2013-11-21 12:55:05 -0800

    Adding reference to blog where I took the idea

--- src/edu/cmu/sv/arm/AsyncTaskCompleteListener.java
+++ src/edu/cmu/sv/arm/AsyncTaskCompleteListener.java
@@ -1,5 +1,7 @@
 package edu.cmu.sv.arm;
 
+//http://www.jameselsey.co.uk/blogs/techblog/extracting-out-your-asynctasks-into-separate-classes-makes-your-code-cleaner/
+	
 public interface AsyncTaskCompleteListener <T>{
 	void onTaskCompleted(T result);
 }

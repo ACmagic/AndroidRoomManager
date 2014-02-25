b67ece3247a78c2bcb866facb79aecf94eedf7c3
Maria Emilia Torino <emilia.torino@west.cmu.edu>
2014-02-25 00:32:07 -0800

    Testing scenarios on parsing configuration file

--- tests/StartActivityControllerTest.java
+++ tests/StartActivityControllerTest.java
@@ -1,39 +1,61 @@
-import static org.junit.Assert.*;
+import junit.framework.TestCase;
 
-//import org.easymock.EasyMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-//import org.powermock.api.easymock.PowerMock;
-//import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.api.mockito.PowerMockito;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowEnvironment;
 
-import android.os.Bundle;
 import android.os.Environment;
-import edu.cmu.sv.arm.ARM;
-import edu.cmu.sv.arm.StartActivity;
 import edu.cmu.sv.arm.StartActivityController;
 import edu.cmu.sv.arm.StartActivityController.ConfigurationStatus;
 
-//@RunWith(RobolectricTestRunner.class)
-//@PrepareForTest(StartActivityController.class)
-public class StartActivityControllerTest{
+@RunWith(RobolectricTestRunner.class)
+@PrepareForTest(StartActivityController.class)
+public class StartActivityControllerTest extends TestCase{
 	
-//	ARM application;
-//	private StartActivityController startActivityController = new StartActivityController(application,
-//			null);
-//	
-//	@Test
-//	public void returnsMediaErrorOnErrorReadingConfigurationFile(){
-//		ShadowEnvironment.setExternalStorageState(Environment.MEDIA_UNMOUNTED);
-//    	assertEquals(ConfigurationStatus.READ_MEDIA_ERROR, startActivityController.configureApplication());
-//	}
-//	
-//	@Test
-//	public void returnsSuccessMsgOnSuccessfullyReadingConfigurationFile(){
-//		ShadowEnvironment.setExternalStorageState(Environment.MEDIA_MOUNTED);
-//		Bundle savedBundle = new Bundle();
-//		StartActivity startActivity = Robolectric.buildActivity( StartActivity.class ).create( savedBundle ).start().resume().get();
-//		
-//		EasyMock.createMockBuilder(StartActivityController.class).addMockedMethod("parseConfiguration").createMock();
-//		assertEquals(ConfigurationStatus.READ_MEDIA_ERROR, startActivityController.configureApplication());
-//	}
+	@Test
+	public void returnsMediaErrorOnErrorReadingConfigurationFile() throws Exception{
+		ShadowEnvironment.setExternalStorageState(Environment.MEDIA_MOUNTED);
+		StartActivityController startActivityController = PowerMockito.spy(new StartActivityController(null,
+				null));
+	    PowerMockito.doReturn(false).when(startActivityController, "parseConfiguration");
+    	assertEquals(ConfigurationStatus.READ_MEDIA_ERROR, startActivityController.configureApplication());
+	}
+	
+	@Test
+	public void returnsMediaErrorOnMediaUnmounted(){
+		StartActivityController startActivityController = new StartActivityController(null,
+				null);
+		ShadowEnvironment.setExternalStorageState(Environment.MEDIA_UNMOUNTED);
+    	assertEquals(ConfigurationStatus.READ_MEDIA_ERROR, startActivityController.configureApplication());
+	}
+	
+	@Test
+	public void returnsUnexpectedErrorOnExceptionParsingFile() throws Exception{
+		ShadowEnvironment.setExternalStorageState(Environment.MEDIA_MOUNTED);
+		StartActivityController startActivityController = PowerMockito.spy(new StartActivityController(null,
+				null));
+	    PowerMockito.doThrow(new Exception()).when(startActivityController, "parseConfiguration");
+    	assertEquals(ConfigurationStatus.UNEXPECTED_ERROR, startActivityController.configureApplication());
+	}
+	
+	@Test
+	public void returnsSuccessMsgOnSuccessfullyReadingConfigurationFile() throws Exception{
+		ShadowEnvironment.setExternalStorageState(Environment.MEDIA_MOUNTED);
+		StartActivityController startActivityController = PowerMockito.spy(new StartActivityController(null,
+				null));
+	    PowerMockito.doReturn(true).when(startActivityController, "parseConfiguration");
+		assertEquals(ConfigurationStatus.USING_CUSTOM_SETTINGS_FILE, startActivityController.configureApplication());
+	}
+	
+	@Test
+	public void returnsSuccessMsgOnSuccessfullyReadingConfigurationFileAndReadOnlyMediaMounted() throws Exception{
+		ShadowEnvironment.setExternalStorageState(Environment.MEDIA_MOUNTED_READ_ONLY);
+		StartActivityController startActivityController = PowerMockito.spy(new StartActivityController(null,
+				null));
+	    PowerMockito.doReturn(true).when(startActivityController, "parseConfiguration");
+		assertEquals(ConfigurationStatus.USING_CUSTOM_SETTINGS_FILE, startActivityController.configureApplication());
+	}
 }
